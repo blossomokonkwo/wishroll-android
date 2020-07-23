c43fe387e88a9588ce5700830107f29af784627d
Great <gboyokonkwo35@gmail.com>
2020-07-22 20:06:36 -0400

    everything I deleted just in case

---
+++ app/src/main/java/co/WishRoll/SplashScreen.java
@@ -0,0 +1,23 @@
+package co.WishRoll;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //in the future
+        //check sharedPreferences if the user is logged in => open home or open login/signup page
+
+
+            startActivity(new Intent(this, WelcomeActivity.class));
+            finish();
+
+
+    }
+}
---
+++ app/src/main/java/co/WishRoll/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package co.WishRoll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        //Join WishRoll page with the pictures and all of that good stuff
+
+    }
+}
