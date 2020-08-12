c797e2f1586c2172a93ccd651bdfe47e811b4708
Great <blossomokonkwo1@gmail.com>
2020-08-11 22:16:14 -0400

    login api response class

---
+++ app/src/main/java/co/wishroll/models/datamodels/LoginResponse.java
@@ -0,0 +1,69 @@
+package co.wishroll.models.datamodels;
+
+
+import androidx.annotation.Nullable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.net.URL;
+import java.util.Date;
+
+public class LoginResponse {
+    //maps the data given by a login POST request
+
+    @Expose
+    @SerializedName("user")
+    private User user;
+
+
+    @Expose
+    @SerializedName("id")
+    private String user_id;
+
+    @Expose
+    @SerializedName("username")
+    private String username;
+
+    @Nullable
+    @Expose
+    @SerializedName("bio")
+    private String bio;
+
+    @Nullable
+    @Expose
+    @SerializedName("profile_background_url")
+    private String profile_background_url;
+
+    @Expose
+    @SerializedName("verified")
+    private boolean verified;
+
+    @Expose
+    @SerializedName("email")
+    private String email;
+
+    @Expose
+    @SerializedName("name")
+    private String name;
+
+    @Expose
+    @SerializedName("avatar")
+    private String avatar;
+
+    
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
--- app/src/main/java/co/wishroll/models/repositories/LoginRepository.java
+++ app/src/main/java/co/wishroll/models/repositories/LoginRepository.java
@@ -1,4 +1,13 @@
 package co.wishroll.models.repositories;
 
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 public class LoginRepository {
+
+
+
+
 }
