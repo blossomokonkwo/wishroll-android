84b9a58d24c48cef3ddbe2e04730bb6b5c4849bc
Great <blossomokonkwo1@gmail.com>
2020-09-25 20:42:04 -0400

    back to square one, leftover classes though, paging with only network from now on, too bad

--- app/src/main/java/co/wishroll/viewmodel/FeedViewModel.java
+++ app/src/main/java/co/wishroll/viewmodel/FeedViewModel.java
@@ -1,21 +1,10 @@
 package co.wishroll.viewmodel;
 
 import androidx.lifecycle.ViewModel;
-import androidx.paging.Pager;
-import androidx.paging.PagingConfig;
-import androidx.paging.PagingData;
-import androidx.paging.rxjava2.PagingRx;
 
-import co.wishroll.localdb.FeedDatabase;
-import co.wishroll.localdb.FeedPostDao;
-import co.wishroll.localdb.FeedPostEntity;
-import co.wishroll.localdb.FeedRemoteMediator;
 import co.wishroll.models.repository.FeedRepository;
-import io.reactivex.Flowable;
-import kotlinx.coroutines.CoroutineScope;
 
 import static co.wishroll.WishRollApplication.applicationGraph;
-import static co.wishroll.WishRollApplication.getContext;
 
 public class FeedViewModel extends ViewModel {
 
@@ -25,16 +14,7 @@ public class FeedViewModel extends ViewModel {
 
     }
 
-    FeedPostDao feedPostDao = FeedDatabase.getInstance(getContext()).discoverPostDao();
 
-    CoroutineScope viewModelScope = ViewModelKt.getViewModelScope(viewModel);
-    Pager<Integer, FeedPostEntity> pager =  new Pager(
-            new PagingConfig(15),
-            null, // initialKey,
-            new FeedRemoteMediator(0), () -> feedPostDao.pagingSource());
-
-    Flowable<PagingData<FeedPostEntity>> flowable = PagingRx.getFlowable(pager);
-    PagingRx.cachedIn(flowable, viewModelScope);
 
 
     //ViewModel for the Feed Fragment, Main Activity
