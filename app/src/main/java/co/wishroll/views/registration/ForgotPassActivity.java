3b3018142444d84db51d39b46b1599e09f55446c
Blossom Okonkwo <blossomokonkwo@Blossoms-MBP-2.fios-router.home>
2021-07-03 16:32:50 -0400

    signup flow

---
+++ app/src/main/java/co/wishroll/views/registration/ForgotPassActivity.java
@@ -0,0 +1,16 @@
+package co.wishroll.views.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import co.wishroll.R;
+
+public class ForgotPassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pass);
+    }
+}
---
+++ app/src/main/java/co/wishroll/views/registration/MutualsActivity.java
@@ -0,0 +1,15 @@
+package co.wishroll.views.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import co.wishroll.R;
+
+public class MutualsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mutuals);
+    }
+}
